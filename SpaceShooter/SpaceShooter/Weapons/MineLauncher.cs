c1dba3425c875358af66a83ca180499a9b48bf3d
karppa1 <K2264@student.jamk.fi>
2016-04-13 17:35:00 +0300

    Add MineLauncher

---
+++ SpaceShooter/SpaceShooter/Weapons/MineLauncher.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using SpaceShooter.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShooter.Weapons
+{
+    class MineLauncher : Weapon
+    {
+        const float mineSpeed = -32;
+        const int magazineSize = 1;
+
+        public override string Name { get { return "MineLauncher"; } }
+        public override double ReloadDelay { get { return 7.0; } }
+        public override double FirerateDelay { get { return 0.05; } }
+
+        public MineLauncher()
+            : base(magazineSize)
+        {
+
+        }
+
+        public override void OnFire(FireEventArgs e)
+        {
+            Mine mine = new Mine(e.Level, e.Position, new Vector2(mineSpeed, 0), e.Shooter.Faction);
+            e.Level.Objects.Add(mine);
+        }
+    }
+}
