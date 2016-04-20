368b9de2bc860a8a14ec5c139de0e0fc2d4dd3d2
karppa1 <K2264@student.jamk.fi>
2016-04-20 19:28:28 +0300

    Add Automatic Rocket Launcher

---
+++ SpaceShooter/SpaceShooter.Core/Weapons/AutomaticRocketLauncher.cs
@@ -0,0 +1,28 @@
+﻿using SpaceShooter.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceShooter.Weapons
+{
+    class AutomaticRocketLauncher : Weapon
+    {
+        const float launchSpeed = 16 * TileSize;
+        const int magazineSize = 10;
+
+        public override string Name { get { return "Automatic Rocket Launcher"; } }
+        public override double ReloadDelay { get { return 1.0; } }
+        public override double FirerateDelay { get { return 0.2; } }
+
+        public AutomaticRocketLauncher()
+            : base(magazineSize)
+        {
+
+        }
+        public override void OnFire(FireEventArgs e)
+        {
+            Rocket rocket = new Rocket(e.Level, e.Position, e.Direction * launchSpeed, e.Shooter.Faction);
+            e.Level.Objects.Add(rocket);
+        }
+    }
+}
