b1d2ef4e2de96ab11af1c9362fca9856abeb8615
VilRan <VilleMattiRantala@gmail.com>
2016-04-25 00:41:04 +0300

    Tweak projetile behavior

--- SpaceShooter/SpaceShooter.Core/Dynamic/Projectiles/Projectile.cs
+++ SpaceShooter/SpaceShooter.Core/Dynamic/Projectiles/Projectile.cs
@@ -6,11 +6,9 @@ namespace SpaceShooter.Dynamic
     public abstract class Projectile : DynamicObject
     {
         public override ObjectCategory Category { get { return ObjectCategory.Projectile; } }
-        public override Vector2 RelativeVelocity { get { return Velocity; } set { Velocity = value - Level.Camera.Velocity; } }
-        public override Vector2 AbsoluteVelocity { get { return Velocity + Level.Camera.Velocity; } }
 
         public Projectile(Texture2D texture, Level level, Vector2 position, Vector2 velocity, float durability, Faction faction)
-            : base(texture, level, position, velocity, durability, faction)
+            : base(texture, level, position, velocity + level.Camera.Velocity, durability, faction)
         {
 
         }
