711ada360392fd65e1439ac3d4ea13ba0d796a44
VilRan <VilleMattiRantala@gmail.com>
2016-04-25 05:15:04 +0300

    Tweak MachinegunDrone

--- SpaceShooter/SpaceShooter.Core/Dynamic/MachineGunDrone.cs
+++ SpaceShooter/SpaceShooter.Core/Dynamic/MachineGunDrone.cs
@@ -10,7 +10,8 @@ namespace SpaceShooter.Dynamic
     {
         const float durability = 200;
         const float collisionDamage = 100;
-        float droneRotation = MathHelper.ToRadians(0);
+        float rotationPhase = 0;
+        float rotationSpeed = MathHelper.PiOver2;
 
         Weapon weapon;
         protected override float CollisionDamage { get { return collisionDamage; } }
@@ -27,26 +28,25 @@ namespace SpaceShooter.Dynamic
         public override void OnUpdate(UpdateEventArgs e)
         {
             weapon.Update(e.GameTime);
-
-            PlayerShip nearestPlayer = GetNearestPlayer();
-            if (nearestPlayer == null)
-                return;
-
-            droneRotation += MathHelper.ToRadians(90f) * (float)e.ElapsedSeconds;
-            Matrix playerMatrix = Matrix.CreateTranslation(new Vector3(nearestPlayer.Position, 0f));
-            Vector2 droneDistance = new Vector2(0, 100);
-            Matrix droneMatrix = Matrix.CreateRotationZ(droneRotation) * playerMatrix;
-            Vector2 nextPosition = Vector2.Transform(droneDistance, droneMatrix);
-            if (e.ElapsedSeconds != 0)
-                Velocity = (nextPosition - Position) / (float)e.ElapsedSeconds;
-            Position = nextPosition;
-            
             DynamicObject target = GetNearest(obj => obj.Faction != Faction && obj.Category == ObjectCategory.Ship);
             if (target != null)
             {
                 Vector2? direction = VectorUtility.FindInterceptDirection(Position, Camera.Velocity, target.Position, target.AbsoluteVelocity, weapon.ProjectileSpeed);
                 if (direction != null)
                     weapon.TryFire(new FireEventArgs(Level, Position, direction.Value, this));
+            }
+
+            PlayerShip nearestPlayer = GetNearestPlayer();
+            if (nearestPlayer != null)
+            {
+                rotationPhase += rotationSpeed * (float)e.ElapsedSeconds;
+                Matrix playerMatrix = Matrix.CreateTranslation(new Vector3(nearestPlayer.Position, 0f));
+                Vector2 droneDistance = new Vector2(0, 100);
+                Matrix droneMatrix = Matrix.CreateRotationZ(rotationPhase) * playerMatrix;
+                Vector2 nextPosition = Vector2.Transform(droneDistance, droneMatrix);
+                if (e.ElapsedSeconds != 0)
+                    Velocity = (nextPosition - Position) / (float)e.ElapsedSeconds;
+                Position = nextPosition;
             }
         }
 
