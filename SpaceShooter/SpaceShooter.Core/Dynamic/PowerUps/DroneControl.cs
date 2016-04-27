81c5bbb87b81509204f0db545abebc6f05b3134d
VilRan <VilleMattiRantala@gmail.com>
2016-04-27 12:34:28 +0300

    Modify powerup appearance

--- SpaceShooter/SpaceShooter.Core/Dynamic/PowerUps/DroneControl.cs
+++ SpaceShooter/SpaceShooter.Core/Dynamic/PowerUps/DroneControl.cs
@@ -6,6 +6,8 @@ namespace SpaceShooter.Dynamic.PowerUps
 {
     class DroneControl : PowerUp
     {
+        protected override float Scale { get { return 0.75f; } }
+
         public DroneControl(Level level, Vector2 position, Vector2 velocity)
             : base(level.Game.Assets.PowerUpTexture, level, position, velocity)
         {
@@ -15,7 +17,6 @@ namespace SpaceShooter.Dynamic.PowerUps
         public override void OnCollision(Collision e)
         {
             base.OnCollision(e);
-            TimedParticle.Emit(Level, Assets.ParticleTexture, e.CollisionPosition, Color.White, 0.25, 1.0, 1024, 20, 40);
             MachinegunDrone machinegunDrone = new MachinegunDrone(Level, Position, Camera.Velocity, e.Other);
             Level.Objects.Add(machinegunDrone);
         }
--- SpaceShooter/SpaceShooter.Core/Dynamic/PowerUps/RepairKit.cs
+++ SpaceShooter/SpaceShooter.Core/Dynamic/PowerUps/RepairKit.cs
@@ -7,10 +7,21 @@ namespace SpaceShooter.Dynamic.PowerUps
 {
     class RepairKit : PowerUp
     {
+        float rotationPhase = 0;
+        
+        protected override float Rotation { get { return rotationPhase; } }
+        protected override float Scale { get { return 0.5f; } }
+
         public RepairKit(Level level, Vector2 position, Vector2 velocity)
             : base(level.Game.Assets.RepairKitTexture, level, position, velocity)
         {
 
+        }
+
+        public override void OnUpdate(UpdateEventArgs e)
+        {
+            base.OnUpdate(e);
+            rotationPhase += MathHelper.PiOver2 * (float)e.ElapsedSeconds;
         }
 
         public override void OnCollision(Collision e)
